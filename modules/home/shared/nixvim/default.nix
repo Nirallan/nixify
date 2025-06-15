476ffdcf160d38799e777bfc581a92bec9d10dfd
IrradiatedRaven <fredrik.stenbraten@outlook.com>
2025-06-15 13:29:49 +0200

    :sparkles: Add wakatime to nixvim + basic options

--- modules/home/shared/nixvim/default.nix
+++ modules/home/shared/nixvim/default.nix
@@ -1,11 +1,44 @@
 {
   inputs,
+  pkgs,
   ...
 }: {
-  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
+  imports = [ 
+    inputs.nixvim.homeManagerModules.nixvim
+    ./wakatime
+  ];
 
   programs.nixvim = {
     enable = true;
     globals.mapleader = " ";
+
+    opts = {
+      number = true;
+      relativenumber = true;
+
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      smartindent = true;
+
+      wrap = false;
+
+      hlsearch = true;
+      incsearch = true;
+      ignorecase = true;
+      smartcase = true;
+
+      cursorline = false;
+
+      termguicolors = true;
+      signcolumn = "yes";
+
+      backspace = "indent,eol,start";
+
+      clipboard = "unnamedplus";
+
+      splitright = true;
+      splitbelow = true;
+    };
   };
 }
---
+++ modules/home/shared/nixvim/wakatime/default.nix
@@ -0,0 +1,5 @@
+_: {
+  programs.nixvim.plugins.wakatime = {
+    enable = true;
+  };
+}
